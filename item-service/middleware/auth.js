369a0383f473d80a496e39c31e5a41b872430681
Nishant Chourasia <m25cse020@iitj.ac.in>
2025-11-13 16:22:50 +0530

    Fix: Add execute permissions for react-scripts in Dockerr

--- item-service/middleware/auth.js
+++ item-service/middleware/auth.js
@@ -1,8 +1,8 @@
-// --- item-service/middleware/auth.js ---
+
 const axios = require('axios');
 
-// Naya: auth-service ka address environment variable se padhein
-// Agar nahi mila, toh default 'http://auth-service:5001' use karein
+
+
 const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || 'http://auth-service:5001';
 
 async function auth(req, res, next) {
@@ -12,16 +12,16 @@ async function auth(req, res, next) {
   }
 
   try {
-    // 1. auth-service ko uske container naam (ya URL) se call karein
+
     const response = await axios.get(`${AUTH_SERVICE_URL}/api/auth/verify-token`, {
       headers: { 'x-auth-token': token }
     });
 
-    // 2. Agar token sahi hai, toh 'auth-service' user ki info bhejega
+
     req.user = response.data; // { id: '...', role: 'admin' }
     next();
   } catch (err) {
-    // Agar 'auth-service' ne error bheja
+
     console.error('Auth middleware error:', err.message);
     res.status(401).json({ msg: 'Token is not valid or auth service is down' });
   }
